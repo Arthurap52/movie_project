4358b4aefe2c111a259270b56aaffb48f72c7028
José Arthur Antunes Pereira <39893001+Arthurap52@users.noreply.github.com>
2023-09-14 23:18:54 -0300

    update styles

--- script.js
+++ script.js
@@ -46,7 +46,6 @@ async function searchPopularMovies() {
             movieCard.innerHTML = `
                 <img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.title}">
                 <h2>${movie.title}</h2>
-                <p>${movie.release_date}</p>
             `;
             movieList.appendChild(movieCard);
         });
